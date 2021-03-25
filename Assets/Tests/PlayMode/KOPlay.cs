9ea5d328eadb958924ee50bad3529319d961dd2f
Kristie O <77216204+known-blue@users.noreply.github.com>
2021-03-25 01:23:09 -0700

    Test Folders

---
+++ Assets/Tests/PlayMode/KOPlay.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using UnityEngine.SceneManagement;
+
+namespace Tests
+{
+    public class KOPlay
+    {
+        // A Test behaves as an ordinary method
+        [OneTimeSetUp]
+        public void LoadScene()
+        {
+            // Use the Assert class to test conditions
+            SceneManager.LoadScene("TestScene");
+        }
+
+        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+        // `yield return null;` to skip a frame.
+        [UnityTest]
+        public IEnumerator GunsLoad()
+        {
+            // Use the Assert class to test conditions.
+            // Use yield to skip a frame.
+            yield return 50;
+            GameObject gun = GameObject.FindWithTag("Weapon");
+            Assert.True(gun.activeInHierarchy);
+        }
+    }
+}
