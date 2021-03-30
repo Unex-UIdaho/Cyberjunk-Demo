19a3c0b1a6fb2d94ab31af5f5d2cfe9b38229030
Sam Frederickson <20883899+SamFrederickson@users.noreply.github.com>
2021-03-29 20:09:33 -0700

    Added edit test SF

---
+++ Assets/Tests/EditMode/SFEditTest.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class SFUnitTests
+    {
+        public float testDamage;
+        [Test]
+        public void GivenDamage()
+        {
+            var go = new GameObject();
+            go.AddComponent<BarrelScript>();
+            testDamage = go.GetComponent<BarrelScript>().barrelDamage;
+            // Debug.Log("Damage:" + testDamage);
+            Assert.IsNotNull(testDamage);
+        }
+    }
+}
